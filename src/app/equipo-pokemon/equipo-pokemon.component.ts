baf4d9872308f1914a6a00cc7f4f7b73a3182caa
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-12-14 11:37:57 +0100

    Obtain and JsonParse item ¡s from LocalStorage

--- src/app/equipo-pokemon/equipo-pokemon.component.ts
+++ src/app/equipo-pokemon/equipo-pokemon.component.ts
@@ -1,3 +1,4 @@
+import { Pokemon } from './../shared/pokemon';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipoPokemonComponent implements OnInit {
 
+  pokemonTeam: Array<Pokemon> = [];
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.obtainAllLocalStorage());
   }
+
+  obtainAllLocalStorage() {
+    let json: Array<JSON> = [] ;
+    var values = [],
+      keys = Object.keys(localStorage),
+      i = keys.length;
+
+    while (i--) {
+      values.push(localStorage.getItem(keys[i]));
+    }
+
+    for(let i = 0; i < values.length; i++){
+      let aux= JSON.parse(values[i]);
+      json.push(aux);
+    }
+    return json;
+  }
+
 
 }
