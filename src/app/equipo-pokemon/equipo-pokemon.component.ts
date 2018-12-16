09405a74ec3bdd101faf171881c582ac44c8f33c
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-12-16 13:44:35 +0100

    Fix PokeTeam appearance

--- src/app/equipo-pokemon/equipo-pokemon.component.ts
+++ src/app/equipo-pokemon/equipo-pokemon.component.ts
@@ -84,35 +84,35 @@ export class EquipoPokemonComponent implements OnInit {
 
     for (let i = 0; i < 6; i++) {
       let auxID = randomIDs[i];
-      let auxName = this.getName(randomIDs[i]);
-      let auxImg = this.getImg(randomIDs[i]);
-      let auxType1 = this.getTypes(randomIDs[i])[0];
-      let auxType2 = this.getTypes(randomIDs[i])[1];
-      let auxMove1 = this.getAbilities(randomIDs[i])[0];
-      let auxMove2 = this.getAbilities(randomIDs[i])[1];
+      let auxName = this.getNameAPI(randomIDs[i]);
+      let auxImg = this.getImgAPI(randomIDs[i]);
+      let auxType1 = this.getTypesAPI(randomIDs[i])[0];
+      let auxType2 = this.getTypesAPI(randomIDs[i])[1];
+      let auxMove1 = this.getAbilitiesAPI(randomIDs[i])[0];
+      let auxMove2 = this.getAbilitiesAPI(randomIDs[i])[1];
       this.pokemonTeam[i] = new Pokemon(auxID, auxName, auxImg, auxType1, auxType2, auxMove1, auxMove2);
     }
 
   }
 
-  getName(id) {
+  getNameAPI(id) {
     this.data.getPokemonImages(id).subscribe(data => this.pokemonName = (data["forms"][0].name));
     return this.pokemonName;
   }
 
-  getTypes(id) {
+  getTypesAPI(id) {
     this.data.getPokemonImages(id).subscribe(data => this.types1.push(data["types"][0]["type"].name));
     this.data.getPokemonImages(id).subscribe(data => this.types2.push(data["types"][1]["type"].name));
     return [this.types1, this.types2];
   }
 
-  getAbilities(id) {
+  getAbilitiesAPI(id) {
     this.data.getPokemonImages(id).subscribe(data => this.moves1.push(data["moves"][0]["move"].name));
     this.data.getPokemonImages(id).subscribe(data => this.moves2.push(data["moves"][1]["move"].name));
     return [this.moves1, this.moves2];
   }
 
-  getImg(id){
+  getImgAPI(id){
     return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id +".png"
   }
 
