9dafb22cd0a764137acd02b256be564bbb2295ef
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-12-17 23:30:46 +0100

    Clean component photo-pokemon

--- src/app/photo-pokemon/photo-pokemon.component.ts
+++ src/app/photo-pokemon/photo-pokemon.component.ts
@@ -9,6 +9,7 @@ import { DataServiceService } from './../services/data-service.service';
   styleUrls: ['./photo-pokemon.component.scss']
 })
 export class PhotoPokemonComponent implements OnInit {
+
   id: string;
   pokemonName: string;
   types1: Array<string> = [];
@@ -23,7 +24,6 @@ export class PhotoPokemonComponent implements OnInit {
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
-    console.log(this.id);
     this.route.params.subscribe(params => this.id = params['id']);
     this.getName();
     this.getTypes();
@@ -32,20 +32,24 @@ export class PhotoPokemonComponent implements OnInit {
     this.setParametersNull();
   }
 
+  //Function that get the name from the API using a service
   getName() {
     this.data.getPokemonImages(this.id).subscribe(data => this.pokemonName = (data["forms"][0].name));
   }
 
+  //Function that get the types from the API using a service
   getTypes() {
     this.data.getPokemonImages(this.id).subscribe(data => this.types1.push(data["types"][0]["type"].name));
     this.data.getPokemonImages(this.id).subscribe(data => this.types2.push(data["types"][1]["type"].name));
   }
 
+  //Function that get the habilities from the API using a service
   getAbilities() {
     this.data.getPokemonImages(this.id).subscribe(data => this.moves1.push(data["moves"][0]["move"].name));
     this.data.getPokemonImages(this.id).subscribe(data => this.moves2.push(data["moves"][1]["move"].name));
   }
 
+  //Function that set all Arrays to "clean"
   setParametersNull(){
     this.moves1 = [];
     this.moves2 = [];
@@ -53,14 +57,17 @@ export class PhotoPokemonComponent implements OnInit {
     this.types2 = [];
   }
 
+  //Function that set the front image of the pokemon to the img var of the component
   frontImg(){
     this.img =  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + this.id +".png"
   }
 
+  //Function that set the back image of the pokemon to the img var of the component
   backImag(){
     this.img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/" + this.id + ".png"
   }
 
+  //Function that get the next ID of the pokemon and load on the pokedex screen
   nextID() {
     var aux = parseInt(this.id) + 1;
     if(aux >= 10090){
@@ -74,6 +81,7 @@ export class PhotoPokemonComponent implements OnInit {
     }
   }
 
+  //Function that get the previus ID of the pokemon and load on the pokedex screen
   prevID() {
     var aux = parseInt(this.id) - 1;
     if(aux >= 1){
@@ -85,9 +93,9 @@ export class PhotoPokemonComponent implements OnInit {
       this.id = aux.toString();
       this.reloadPokemon();
     }
-
   }
 
+  //Function that reload the next or the previus pokemon of the pokedex
   reloadPokemon(){
     this.getName();
     this.getTypes();
@@ -96,6 +104,7 @@ export class PhotoPokemonComponent implements OnInit {
     this.setParametersNull();
   }
 
+  //Function that adds the pokemon to the LocalStorage, only if it is not inside yet
   AddPokemonToTeam() {
    this.teamItem = new Pokemon(this.id, this.pokemonName, this.img, this.types1[0], this.types2[0], this.moves1[0], this.moves2[0]);
    console.log(this.teamItem);
